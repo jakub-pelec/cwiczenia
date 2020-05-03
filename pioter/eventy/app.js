6a4618d76dd413b116365cc8f3c7988dde51dca5
cichowlasp <cichowlas.p@gmail.com>
2020-05-03 22:44:46 +0200

    tibijski update stronki

--- pioter/eventy/app.js
+++ pioter/eventy/app.js
@@ -1,5 +1,14 @@
 const input = document.querySelector("#input");
-
+const licznik = document.querySelector("#licznik");
+const button = document.querySelector("#przycisk");
+const text = document.querySelector("#tekst");
 input.addEventListener("input",function (event){
-    console.log(event.target.value)
+    text.innerHTML = event.target.value;
 });
+
+button.addEventListener("click", function (event){
+    liczba = parseInt(licznik.innerHTML);
+    liczba++;
+    licznik.innerHTML = liczba;
+});
+
