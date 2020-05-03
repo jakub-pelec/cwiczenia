11d491336a2e26696801fbdeffebab2ffc01e693
cichowlasp <cichowlas.p@gmail.com>
2020-05-03 23:10:00 +0200

    jeszcze bardziej tibijski update

--- pioter/eventy/app.js
+++ pioter/eventy/app.js
@@ -2,6 +2,12 @@ const input = document.querySelector("#input");
 const licznik = document.querySelector("#licznik");
 const button = document.querySelector("#przycisk");
 const text = document.querySelector("#tekst");
+const clear = document.querySelector("#clear");
+
+clear.addEventListener("click",(event) => {
+    licznik.innerHTML = 0;
+});
+
 input.addEventListener("input",function (event){
     text.innerHTML = event.target.value;
 });
