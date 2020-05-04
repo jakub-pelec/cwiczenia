0eaaf0ef865da90b1c846530a8a762d9d3378ea7
cichowlasp <cichowlas.p@gmail.com>
2020-05-04 18:43:50 +0200

    jakieś pierdy

--- pioter/eventy/app.js
+++ pioter/eventy/app.js
@@ -3,13 +3,11 @@ const licznik = document.querySelector("#licznik");
 const button = document.querySelector("#przycisk");
 const text = document.querySelector("#tekst");
 const clear = document.querySelector("#clear");
+const submit = document.querySelector("#submit");
+let textToShow;
 
 clear.addEventListener("click",(event) => {
     licznik.innerHTML = 0;
-});
-
-input.addEventListener("input",function (event){
-    text.innerHTML = event.target.value;
 });
 
 button.addEventListener("click", function (event){
@@ -18,3 +16,11 @@ button.addEventListener("click", function (event){
     licznik.innerHTML = liczba;
 });
 
+input.addEventListener("input",(event)=>{
+    textToShow = event.target.value;
+});
+
+submit.addEventListener("click",function (event){
+    text.innerHTML = textToShow;
+    input.value = "";
+});
