6b902cb173299907f14831865092355cc5879662
cichowlasp <cichowlas.p@gmail.com>
2020-05-15 02:15:55 +0200

    updated syntax

--- pioter/cwiczonko/app.js
+++ pioter/cwiczonko/app.js
@@ -1,19 +1,19 @@
 const readline = require("readline-sync");
-const {v4: uuidv4} = require('uuid');
+const uuid = require('uuid');
 
 const generateObject = () => {
     const name = readline.question("What's your name? : ");
     const surname = readline.question("What's your surname? : ");
     const age = readline.question("How old are you? : ");
-    const city = readline.question("Where have you been born? : ");
-    const uuid =  uuidv4();
+    const whereBorn = readline.question("Where have you been born? : ");
+    const id = uuid.v4();
     return {
-        uuid: uuid,
+        id,
         data: {
-            name: name,
-            surname: surname,
-            age: age,
-            whereBorn: city
+            name,
+            surname,
+            age,
+            whereBorn,
         }
     };
 };
